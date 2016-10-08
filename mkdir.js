a966435c25130c5959ccfd6de1d3fa05e0a894d8
rus1978 <BondarRuslanVladimirovich@gmail.com>
2016-10-08 15:04:13 +0300

    Update mkdir.js

--- mkdir.js
+++ mkdir.js
@@ -1,7 +1,7 @@
     var fs = require('fs');
     function mkdir(s_dir) {
         var hereDir = '';
-        s_dir.split('/').forEach(function (item, i) {
+        s_dir.split('/').forEach(function (item) {
             hereDir += item + '/';
             if (!fs.existsSync(hereDir))fs.mkdirSync(hereDir);
         });
